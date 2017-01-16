54199021dc6b97d2e77da5d4e702772e59b0d8b7
Joël Schaerer <joel.schaerer@laposte.net>
2017-01-16 09:28:00 +0100

    softmax

--- MyBot.py
+++ MyBot.py
@@ -5,7 +5,7 @@ import pickle
 import logging
 import hlt
 from hlt import NORTH, EAST, SOUTH, WEST, STILL, Move
-import numpy
+import numpy as np
 
 logging.basicConfig(format='%(asctime)-15s %(message)s',
         level=logging.INFO, filename="bot.log")
@@ -13,6 +13,11 @@ logging.basicConfig(format='%(asctime)-15s %(message)s',
 possible_moves = [NORTH, EAST, SOUTH, WEST, STILL]
 myID, game_map = hlt.get_init()
 hlt.send_init("MyPythonBot")
+
+def softmax(x):
+    e_x = np.exp(x - np.max(x))
+    out = e_x / e_x.sum()
+    return out
 
 #Square = namedtuple('Square', 'x y owner strength production')
 
@@ -25,13 +30,15 @@ class Model(object):
             logging.info("Generating random weights...")
             input_length = 9
             hidden_length = 20
-            self.hidden = numpy.random.normal(size=(input_length,  hidden_length))
-            self.output = numpy.random.normal(size=(hidden_length, 5))
+            self.hidden = np.random.normal(size=(input_length,  hidden_length))
+            self.hidden /= len(self.hidden)
+            self.output = np.random.normal(size=(hidden_length, 5))
+            self.output /= len(self.output)
             pickle.dump((self.hidden, self.output), open("weights.pck", "wb"))
     def relu(self, vector):
-        return numpy.maximum(vector,0,vector)
+        return np.maximum(vector,0,vector)
     def predict(self, input):
-        return self.relu(input @ self.hidden) @ self.output
+        return softmax(self.relu(input @ self.hidden) @ self.output)
 
 model = Model()
 while True:
@@ -44,7 +51,7 @@ while True:
         op_values = [n.strength if n.owner!=myID else 0 for n in neighbors]
         # productions =
         my_strength = [square.strength]
-        model_input = numpy.array(my_values+op_values+my_strength)
+        model_input = np.array(my_values+op_values+my_strength)
         logging.info(model.predict(model_input))
         return model_input
 
@@ -53,9 +60,9 @@ while True:
             return random.choice((NORTH, EAST, SOUTH, WEST, STILL))
         else:
             return STILL
-    # inputs = numpy.vstack(gen_input(square) for square in game_map if square.owner == myID)
+    # inputs = np.vstack(gen_input(square) for square in game_map if square.owner == myID)
     # logging.info(model.predict(inputs))
     #moves = [Move(square, random.choice((NORTH, EAST, SOUTH, WEST, STILL))) for square in game_map if square.owner == myID]
     #moves = [Move(square, gen_move(square)) for square in game_map if square.owner == myID]
-    moves = [Move(square, numpy.argmax(model.predict(gen_input(square)))) for square in game_map if square.owner == myID]
+    moves = [Move(square, np.argmax(model.predict(gen_input(square)))) for square in game_map if square.owner == myID]
     hlt.send_frame(moves)
